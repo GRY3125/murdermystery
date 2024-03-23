9365e672222d1ad10cc700a369e6703b5cdd7eeb
GRY3125 <ruyinggan@gmail.com>
2024-03-23 09:52:50 +0800

    ?????

---
+++ script.js
@@ -0,0 +1,12 @@
+var evidencechecked=[] 
+var noofevidence=0
+var suscmd=0
+
+var inventorylist=["apple", "autopsy report"]
+var currentplace=13 
+
+var fleshdiscovered=0
+var allevidencediscovered=0 
+
+document.getElementById('boldtext').style.fontWeight = "bold";
+document.getElementById("intro").innerHTML = "\n\n"+bold("The Murder Of James Brown").bold()+"asghjkl";
