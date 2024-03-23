c30907af5a7371389cbc65b69b08c094ace724a8
GRY3125 <ruyinggan@gmail.com>
2024-03-23 10:58:35 +0800

    ...

--- script.js
+++ script.js
@@ -8,5 +8,3 @@ var currentplace=13
 var fleshdiscovered=0
 var allevidencediscovered=0 
 
-document.getElementById('boldtext').style.fontWeight = "bold";
-document.getElementById("intro").innerHTML = "\n\n"+bold("The Murder Of James Brown").bold()+"asghjkl";
